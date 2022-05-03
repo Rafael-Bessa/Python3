bb2d29b65225af827e3f1757089cffeec3b269c3
Rafael Bessa <rafaelbessacivil@gmail.com>
2022-05-03 18:14:50 -0300

    Exercício sobre o Triângulo de Floyd

---
+++ triangulo_floyd.py
@@ -0,0 +1,15 @@
+print("*************************")
+
+n = int(input("Qual número? >>> "))
+j = 0
+m = 1
+
+for i in range (1,n+1):
+    for j in range(1,i+1):
+        print(m,end=" ")
+        m += 1
+    print()
+
+
+
+
