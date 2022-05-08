33644800dbe19d2a1efd7653e2ac68c34b4fbd2b
Rafael Bessa <rafaelbessacivil@gmail.com>
2022-05-07 22:45:35 -0300

    Algoritmo para sen(x)

---
+++ seno.py
@@ -0,0 +1,30 @@
+import math
+
+print(30*"*")
+print("Programa para calcular o \033[1:31mSeno\033[m de um ângulo entre 0° e 360°")
+print("Quanto maior o número de iterações, maior a precisão do valor")
+print(30*"~")
+
+x = float(input("Qual ângulo? >>> "))
+
+if(x > 360 and x < 720):
+    x = x - 360
+
+angulo = (x*math.pi)/180
+float(angulo)
+soma = angulo
+contador = 0
+
+
+n = int(input("Quantas iterações? >>> "))
+
+for i in range(3,1000000000,2):
+    contador += 1
+    soma = soma + (((-1)**contador)*(angulo**i)/math.factorial(i))
+    if(contador == n):
+        break
+
+
+if(soma < 0.001 and soma > 0):
+    soma = 0
+print(f"O \033[1:31mSeno\033[m do ângulo é \033[1:32m{soma}")
