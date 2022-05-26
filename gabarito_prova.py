f2ceaab95812d04207ae975ff0c45d9bd94481c5
Rafael Bessa <rafaelbessacivil@gmail.com>
2022-05-26 16:44:53 -0300

    Ajustes no código, legivel

--- gabarito_prova.py
+++ gabarito_prova.py
@@ -1,8 +1,8 @@
-"""Desenvolver um programa para verificar a nota do aluno em uma prova com 10 questões, o programa deve perguntar ao aluno a resposta de cada questão e ao final comparar com o gabarito da prova e assim calcular o total de acertos e a nota (atribuir 1 ponto por resposta certa). Após cada aluno utilizar o sistema deve ser feita uma pergunta se outro aluno vai utilizar o sistema. Após todos os alunos terem respondido informar:
-- Maior e Menor Acerto;
-- Total de Alunos que utilizaram o sistema;
-- A Média das Notas da Turma.
-- Gabarito da Prova:
+"""Desenvolver um programa para verificar a nota do aluno em uma prova com 10 questões, o programa deve perguntar ao
+aluno a resposta de cada questão e ao final comparar com o gabarito da prova e assim calcular o total de acertos e a
+nota (atribuir 1 ponto por resposta certa). Após cada aluno utilizar o sistema deve ser feita uma pergunta se outro
+aluno vai utilizar o sistema. Após todos os alunos terem respondido informar: - Maior e Menor Acerto; - Total de
+Alunos que utilizaram o sistema; - A Média das Notas da Turma. - Gabarito da Prova:
 
 01 - A
 02 - B
@@ -15,8 +15,8 @@
 09 - B
 10 - A"""
 
-print(30*"*")
-print(30*"~")
+print(30 * "*")
+print(30 * "~")
 respostas = []
 gabarito = "ABCDEEDCBA"
 acertou = 0
@@ -28,28 +28,26 @@ while usando:
     nome = input("Qual o nome do aluno? >>> ")
     nome = nome.title()
 
-    for nota in range(1,11):
+    for nota in range(1, 11):
         respostas.append(input(f"O que colocou na questão {nota} ? >>> "))
         respostas_arrumadas = "".join(respostas)
         respostas_arrumadas = respostas_arrumadas.upper()
 
-        if(gabarito[nota-1] == respostas_arrumadas[nota-1]):
+        if gabarito[nota - 1] == respostas_arrumadas[nota - 1]:
             print(f"Acertou a questão de número {nota}")
             acertou += 1
         else:
             print(f"Errou a questão de número {nota}")
 
-
-
     print(f"O aluno \033[1:32m{nome}\033[m acertou ao todo {acertou} questão(ões)")
     print()
     continuar = input("Outro aluno vai usar o programa agora? Digite 1 para SIM ou 2 para NÃO >>> ")
-    if(continuar == "1"):
+    if continuar == "1":
         usando = True
-        banco_dados.append((nome,acertou))
+        banco_dados.append((nome, acertou))
         acertou = 0
         respostas = []
-    elif(continuar == "2"):
+    elif continuar == "2":
         usando = False
         banco_dados.append((nome, acertou))
     else:
@@ -62,7 +60,7 @@ print()
 print(banco_dados)
 print(f"O número de alunos que usaram o sistema foi \033[1:32m{len(banco_dados)}\033[m")
 
-for i in range(0,len(banco_dados)):
+for i in range(0, len(banco_dados)):
     media = media + (banco_dados[i][1])
 
-print(f"E a média das notas foi: \033[1:32m{media/len(banco_dados)}\033[m")
+print(f"E a média das notas foi: \033[1:32m{media / len(banco_dados)}\033[m")
