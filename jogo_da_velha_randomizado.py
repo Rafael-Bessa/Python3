ce686c506746991c0b5e0f1e332dab76beafe16d
Rafael Bessa <rafaelbessacivil@gmail.com>
2022-05-26 12:51:35 -0300

    Jogo arrumado, funcionando perfeitamente

--- jogo_da_velha_randomizado.py
+++ jogo_da_velha_randomizado.py
@@ -1,175 +1,146 @@
 import random
 
-a = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
-
-jogada = 0
-velha = False
+a = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
 
 
-def computador(jogada, velha):
+def computador():
     if (a[0][0] == a[0][1] == a[0][2] or a[1][0] == a[1][1] == a[1][2] or a[2][0] == a[2][1] == a[2][2]) or \
             a[0][0] == a[1][0] == a[2][0] or a[0][1] == a[1][1] == a[2][1] or a[0][2] == a[1][2] == a[2][2] or \
             a[0][0] == a[1][1] == a[2][2] or a[0][2] == a[1][1] == a[2][0]:
         print("\033[1:31mAcabou\033[m")
-        velha = True
+        print("\033[1:36mHumano Venceu\033[m")
+
     else:
-
-        '''if (jogada == 9):
-            velha = True
-            print("Acabou aqui")
-            print(velha)'''
-
-        # else:
+        jogo = True
         pc = (a[random.randrange(0, 3)][random.randrange(0, 3)])
-        while pc == "O" or pc == "X":
-            pc = (a[random.randrange(0, 3)][random.randrange(0, 3)])
-        else:
 
-            if 4 > int(pc) > 0:
+        if pc == "O" or pc == "X":
+            if ((a[0][0] == "X" or a[0][0] == "O") and (a[0][1] == "X" or a[0][1] == "O") and
+                    (a[0][2] == "X" or a[0][2] == "O") and (a[1][0] == "X" or a[1][0] == "O")
+                    and (a[1][1] == "X" or a[1][1] == "O") and (a[1][2] == "X" or a[1][2] == "O") and (
+                            a[2][0] == "X" or a[2][0] == "O") and (a[2][1] == "X" or a[2][1] == "O")
+                    and (a[2][2] == "X" or a[2][2] == "O")):
+                print("\033[1:31mDEU VELHA!\033[m")
+                jogo = False
+
+            else:
+                while True:
+                    pc = (a[random.randrange(0, 3)][random.randrange(0, 3)])
+                    if pc == "O" or pc == "X":
+                        continue
+                    else:
+                        pc = int(pc)
+                        break
+        if jogo:
+            pc = int(pc)
+            if 4 > pc > 0:
                 if int(pc) == 1:
                     a[0][0] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
                 elif 2 == int(pc):
                     a[0][1] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
                 else:
                     a[0][2] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
 
-            elif 3 < int(pc) < 7:
+            elif 3 < pc < 7:
                 if int(pc) == 4:
                     a[1][0] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
 
                 elif int(pc) == 5:
                     a[1][1] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
                 else:
                     a[1][2] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
 
             elif 6 < int(pc) < 10:
                 if 7 == int(pc):
                     a[2][0] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
                 elif 8 == int(pc):
                     a[2][1] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
+                    pessoa()
                 else:
                     a[2][2] = "X"
                     print(*a, sep="\n")
-                    jogada += 1
-                    pessoa(jogada, velha)
-
-        return jogada
-    return velha
+                    pessoa()
 
 
-def pessoa(jogada, velha):
+def pessoa():
     if (a[0][0] == a[0][1] == a[0][2] or a[1][0] == a[1][1] == a[1][2] or a[2][0] == a[2][1] == a[2][2]) or \
             a[0][0] == a[1][0] == a[2][0] or a[0][1] == a[1][1] == a[2][1] or a[0][2] == a[1][2] == a[2][2] or \
             a[0][0] == a[1][1] == a[2][2] or a[0][2] == a[1][1] == a[2][0]:
         print("\033[1:31mAcabou\033[m")
-        velha = True
+        print("\033[1:34mComputador Venceu\033[m")
 
-    if jogada == 9:
-        velha = True
-        print(velha)
-        print("Acabou la")
     else:
+        print("Onde você quer jogar? ")
+        print(*a, sep="\n")
+        print("Digite o número do local desejado ")
+        x = int(input("Já pensou? Digite aqui >>> "))
 
-        # velha == False
-        while not velha:
-            print("Onde você quer jogar? ")
-            print(*a, sep="\n")
-            print("Digite o número do local desejado ")
-            x = int(input("Já pensou? Digite aqui >>> "))
+        if 4 > x > 0:
+            if x == 1:
+                a[0][0] = a[0][0].replace("1", "O")
+                print(*a, sep="\n")
+                print()
+                computador()
+            elif x == 2:
+                a[0][1] = a[0][1].replace("2", "O")
+                print(*a, sep="\n")
+                print()
+                computador()
+            else:
+                a[0][2] = a[0][2].replace("3", "O")
+                print(*a, sep="\n")
+                print()
+                computador()
 
-            if 4 > x > 0:
-                if x == 1:
-                    a[0][0] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
-                elif x == 2:
-                    a[0][1] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
-                else:
-                    a[0][2] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
+        elif 3 < x < 7:
+            if x == 4:
+                a[1][0] = "O"
+                print(*a, sep="\n")
+                print()
+                computador()
 
-            elif 3 < x < 7:
-                if x == 4:
-                    a[1][0] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
+            elif x == 5:
+                a[1][1] = "O"
+                print(*a, sep="\n")
+                print()
+                computador()
+            else:
+                a[1][2] = "O"
+                print(*a, sep="\n")
+                print()
+                computador()
 
-                elif x == 5:
-                    a[1][1] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
-                else:
-                    a[1][2] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
-
-            elif 6 < x < 10:
-                if x == 7:
-                    a[2][0] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
-                elif x == 8:
-                    a[2][1] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
-                else:
-                    a[2][2] = "O"
-                    print(*a, sep="\n")
-                    print()
-                    jogada += 1
-                    computador(jogada, velha)
-
-            if (a[0][0] == a[0][1] == a[0][2] or a[1][0] == a[1][1] == a[1][2] or a[2][0] == a[2][1] == a[2][2]) or \
-                    a[0][0] == a[1][0] == a[2][0] or a[0][1] == a[1][1] == a[2][1] or a[0][2] == a[1][2] == a[2][2] or \
-                    a[0][0] == a[1][1] == a[2][2] or a[0][2] == a[1][1] == a[2][0]:
-                print("\033[1:31mAcabou\033[m")
-                break
-
-        return jogada
-    return velha
+        elif 6 < x < 10:
+            if x == 7:
+                a[2][0] = "O"
+                print(*a, sep="\n")
+                print()
+                computador()
+            elif x == 8:
+                a[2][1] = "O"
+                print(*a, sep="\n")
+                print()
+                computador()
+            else:
+                a[2][2] = "O"
+                print(*a, sep="\n")
+                print()
+                computador()
 
 
-pessoa(jogada, velha)
+pessoa()
