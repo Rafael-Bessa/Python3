0876284f75495a354b1075046c377b988751a277
Rafael Bessa <rafaelbessacivil@gmail.com>
2022-06-01 12:43:21 -0300

    Formatei o código

--- Numeros_Primos.py
+++ Numeros_Primos.py
@@ -5,15 +5,15 @@ i = 1
 
 numero = int(input("Qual número? >>> "))
 
-while(i <= numero):
-    if(numero % i == 0):
+while i <= numero:
+    if numero % i == 0:
         print(f"\033[1:33mDividiu por {i}")
         divisores += 1
         i += 1
     else:
         i += 1
 
-if(i == numero + 1 and divisores == 2):
+if i == numero + 1 and divisores == 2:
     print("\033[1:32mÉ Primo")
 else:
     print("\033[1:31mNão é Primo")
