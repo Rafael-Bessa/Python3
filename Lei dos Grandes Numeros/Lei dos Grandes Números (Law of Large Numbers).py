ed1106513947e43196154951a5ef06915262fa04
Rafael Bessa <rafaelbessacivil@gmail.com>
2022-06-08 18:29:08 -0300

    Formatando o código corretamente

--- Lei dos Grandes Numeros/Lei dos Grandes Números (Law of Large Numbers).py
+++ Lei dos Grandes Numeros/Lei dos Grandes Números (Law of Large Numbers).py
@@ -3,21 +3,20 @@ import random
 print()
 print("Lei dos Grandes Números (Law of Large Numbers)")
 print()
-print("**************************")
+print("*"*50)
 print()
 
-soma_de_lançamentos = 0
+soma_de_lancamentos = 0
 i = 1
 media = 0
 
-n = int(input("Quantos vezes você vai lançar o dado? "))
+n = int(input("Quantos vezes você vai lançar o dado? >>> "))
 
-while(i <= n):
-    valor_lançamento = random.randrange(1,7)
-    soma_de_lançamentos = soma_de_lançamentos + valor_lançamento
+while i <= n:
+    valor_lancamento = random.randrange(1, 7)
+    soma_de_lancamentos += valor_lancamento
     i += 1
 
-
-media = (soma_de_lançamentos/n)
-print("A média aritmética de {} lançamentos é {}".format(n,media))
+media = (soma_de_lancamentos / n)
+print("A média aritmética de {} lançamentos é \033[1:36m{}\033[m".format(n, media))
 print("Quanto maior o número de lançamentos é possível concluir que a média converge para o número 3.5")
